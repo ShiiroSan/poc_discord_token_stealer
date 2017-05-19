5d299d2a79c3d162edefeed202f9e4d3b6b9454f
Anaë Yuurei <eaudrey96@gmail.com>
2017-05-19 10:36:47 +0200

    Ajoutez des fichiers projet.

---
+++ PoC_Discord_Token_Stealer/stdafx.cpp
@@ -0,0 +1,8 @@
+// stdafx.cpp : fichier source incluant simplement les fichiers Include standard
+// PoC_Discord_Token_Stealer.pch représente l'en-tête précompilé
+// stdafx.obj contient les informations de type précompilées
+
+#include "stdafx.h"
+
+// TODO: faites référence aux en-têtes supplémentaires nécessaires dans STDAFX.H
+// absents de ce fichier
